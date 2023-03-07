9e707098d1b742c20b12f17c8df3cd42e7d6519f
John Valentino <jvalentino2@gmail.com>
2023-03-07 06:42:34 -0600

    Added fluentbit to openjdk container

--- config/docker/start.sh
+++ config/docker/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /opt/fluent-bit/bin
+./fluent-bit -c fluentbit.conf > fluentbit.log 2>&1 &
+
 cd /usr/local
-echo 4
 java -jar -Dspring.datasource.url=jdbc:postgresql://pg-primary-postgresql:5432/examplesys sys-golf-rest-0.0.1.jar
